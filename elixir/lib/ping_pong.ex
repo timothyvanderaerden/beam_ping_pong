ab1ad9524305d46d6843a1cc157f567080d7bed2
Timothy Vanderaerden <timothy.vanderaerden@sippy.cloud>
2021-08-22 16:44:21 +0200

    [Elixir] Add supervisor

--- elixir/lib/ping_pong.ex
+++ elixir/lib/ping_pong.ex
@@ -1,4 +1,17 @@
 defmodule PingPong do
+  use GenServer
+
+  def start_link(state) do
+    GenServer.start_link(__MODULE__, state, name: __MODULE__)
+  end
+
+  @impl true
+  def init(state) do
+    Process.register(spawn(fn -> pong() end), :pong_process)
+    Process.register(spawn(fn -> ping(3) end), :ping_process)
+    {:ok, state}
+  end
+
   def ping(0) do
     send(:pong_process, :finished)
     IO.puts("Ping finished")
@@ -25,10 +38,5 @@ defmodule PingPong do
         send(:ping_process, :pong)
         pong()
     end
-  end
-
-  def start do
-    Process.register(spawn(fn -> pong() end), :pong_process)
-    Process.register(spawn(fn -> ping(3) end), :ping_process)
   end
 end
--- elixir/mix.exs
+++ elixir/mix.exs
@@ -14,7 +14,8 @@ defmodule PingPong.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod: {PingPong.Application, []}
     ]
   end
 
